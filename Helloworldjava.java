9180e0280787d24c8629354c4e86cb3869def4cf
Aritra Biswas <aritrabiswas586@gmail.com>
2024-09-02 13:02:53 +0530

    Update Helloworldjava.java

--- Helloworldjava.java
+++ Helloworldjava.java
@@ -2,6 +2,6 @@ class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello and welcome to the world");
+		System.out.println("Hello and welcome to the world 2 ");
 	}
 }	
